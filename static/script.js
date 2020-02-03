b175c8da5502b45fbe80bc5b37bc54e517c5c416
Hans Wustrack <hans2792@gmail.com>
2020-02-03 20:44:07 +1300

    city and location filtering working

--- static/script.js
+++ static/script.js
@@ -26,44 +26,290 @@ const COMPANIES = {
     }
 };
 
-var JOBS = [];
+var JOBS = [{
+    "company": "betterment",
+    "size": "sm",
+    "industry": "fin",
+    "title": "Affiliate Marketing Manager ",
+    "location": "New York, New York",
+    "absolute_url": "https://boards.greenhouse.io/betterment/jobs/1985674?gh_jid=1985674"
+}, {
+    "company": "betterment",
+    "size": "sm",
+    "industry": "fin",
+    "title": "Broker/Dealer Operations Associate ",
+    "location": "New York, New York",
+    "absolute_url": "https://boards.greenhouse.io/betterment/jobs/2033122?gh_jid=2033122"
+}, {
+    "company": "betterment",
+    "size": "sm",
+    "industry": "fin",
+    "title": "Compliance Associate ",
+    "location": "Betterment HQ - New York City",
+    "absolute_url": "https://boards.greenhouse.io/betterment/jobs/2015120?gh_jid=2015120"
+}, {
+    "company": "betterment",
+    "size": "sm",
+    "industry": "fin",
+    "title": "Customer Experience Associate (Denver)",
+    "location": "Betterment - Denver",
+    "absolute_url": "https://boards.greenhouse.io/betterment/jobs/2006926?gh_jid=2006926"
+}, {
+    "company": "betterment",
+    "size": "sm",
+    "industry": "fin",
+    "title": "Don’t see your dream job? Apply here to join our talent community!",
+    "location": "Betterment HQ - NYC",
+    "absolute_url": "https://boards.greenhouse.io/betterment/jobs/1741642?gh_jid=1741642"
+}, {
+    "company": "betterment",
+    "size": "sm",
+    "industry": "fin",
+    "title": "Engineering Manager - Backend",
+    "location": "New York City",
+    "absolute_url": "https://boards.greenhouse.io/betterment/jobs/1725485?gh_jid=1725485"
+}, {
+    "company": "betterment",
+    "size": "sm",
+    "industry": "fin",
+    "title": "Freelance Creative Project Coordinator ",
+    "location": "New York, New York",
+    "absolute_url": "https://boards.greenhouse.io/betterment/jobs/2035407?gh_jid=2035407"
+}, {
+    "company": "betterment",
+    "size": "sm",
+    "industry": "fin",
+    "title": "Head of Sales",
+    "location": "Betterment HQ - New York City",
+    "absolute_url": "https://boards.greenhouse.io/betterment/jobs/2004366?gh_jid=2004366"
+}, {
+    "company": "betterment",
+    "size": "sm",
+    "industry": "fin",
+    "title": "Lead Security Engineer",
+    "location": "Betterment HQ - New York City",
+    "absolute_url": "https://boards.greenhouse.io/betterment/jobs/2055995?gh_jid=2055995"
+}, {
+    "company": "betterment",
+    "size": "sm",
+    "industry": "fin",
+    "title": "Legal Counsel",
+    "location": "Betterment HQ - New York City",
+    "absolute_url": "https://boards.greenhouse.io/betterment/jobs/2065087?gh_jid=2065087"
+}, {
+    "company": "betterment",
+    "size": "sm",
+    "industry": "fin",
+    "title": "Mobile Engineer - Android ",
+    "location": "Betterment HQ - New York City",
+    "absolute_url": "https://boards.greenhouse.io/betterment/jobs/2061693?gh_jid=2061693"
+}, {
+    "company": "betterment",
+    "size": "sm",
+    "industry": "fin",
+    "title": "Mobile Engineering Manager",
+    "location": "New York, NY",
+    "absolute_url": "https://boards.greenhouse.io/betterment/jobs/1821512?gh_jid=1821512"
+}, {
+    "company": "betterment",
+    "size": "sm",
+    "industry": "fin",
+    "title": "Retail Analytics Leader",
+    "location": "Betterment HQ - New York City",
+    "absolute_url": "https://boards.greenhouse.io/betterment/jobs/2014451?gh_jid=2014451"
+}, {
+    "company": "betterment",
+    "size": "sm",
+    "industry": "fin",
+    "title": "Senior Product Designer",
+    "location": "Betterment HQ - New York City",
+    "absolute_url": "https://boards.greenhouse.io/betterment/jobs/2003772?gh_jid=2003772"
+}, {
+    "company": "betterment",
+    "size": "sm",
+    "industry": "fin",
+    "title": "Senior Product Marketing Associate",
+    "location": "Betterment HQ - New York City",
+    "absolute_url": "https://boards.greenhouse.io/betterment/jobs/2036554?gh_jid=2036554"
+}, {
+    "company": "betterment",
+    "size": "sm",
+    "industry": "fin",
+    "title": "Site Reliability Engineer",
+    "location": "Betterment HQ - New York City",
+    "absolute_url": "https://boards.greenhouse.io/betterment/jobs/2035452?gh_jid=2035452"
+}, {
+    "company": "betterment",
+    "size": "sm",
+    "industry": "fin",
+    "title": "Social Media Designer ",
+    "location": "New York, New York",
+    "absolute_url": "https://boards.greenhouse.io/betterment/jobs/2031868?gh_jid=2031868"
+}, {
+    "company": "betterment",
+    "size": "sm",
+    "industry": "fin",
+    "title": "Sr. Compliance Associate (Broker-Dealer)",
+    "location": "New York, New York",
+    "absolute_url": "https://boards.greenhouse.io/betterment/jobs/1933710?gh_jid=1933710"
+}, {
+    "company": "betterment",
+    "size": "sm",
+    "industry": "fin",
+    "title": "Strategic Operations Manager ",
+    "location": "Betterment HQ - New York City",
+    "absolute_url": "https://boards.greenhouse.io/betterment/jobs/2055916?gh_jid=2055916"
+}, {
+    "company": "betterment",
+    "size": "sm",
+    "industry": "fin",
+    "title": "Talent & Development Program Intern - Spring 2020",
+    "location": "Betterment HQ - New York City",
+    "absolute_url": "https://boards.greenhouse.io/betterment/jobs/2001096?gh_jid=2001096"
+}, {
+    "company": "thrillist",
+    "size": "med",
+    "industry": "media",
+    "title": "Commerce Editor",
+    "location": "New York, NY",
+    "absolute_url": "https://boards.greenhouse.io/thrillist/jobs/2061792"
+}, {
+    "company": "thrillist",
+    "size": "med",
+    "industry": "media",
+    "title": "Director of Audience Development, Thrillist ",
+    "location": "New York, NY",
+    "absolute_url": "https://boards.greenhouse.io/thrillist/jobs/2006232"
+}, {
+    "company": "thrillist",
+    "size": "med",
+    "industry": "media",
+    "title": "Office Production Assistant ",
+    "location": "New York, NY",
+    "absolute_url": "https://boards.greenhouse.io/thrillist/jobs/2002379"
+}, {
+    "company": "thrillist",
+    "size": "med",
+    "industry": "media",
+    "title": "Senior Designer, Thrillist ",
+    "location": "New York, NY",
+    "absolute_url": "https://boards.greenhouse.io/thrillist/jobs/2006035"
+}, {
+    "company": "thrillist",
+    "size": "med",
+    "industry": "media",
+    "title": "Staff Editor, Food & Travel",
+    "location": "Los Angeles, CA",
+    "absolute_url": "https://boards.greenhouse.io/thrillist/jobs/1273769"
+}, {
+    "company": "reddit",
+    "size": "med",
+    "industry": "tech",
+    "title": "Ad Ops Specialist",
+    "location": "New York",
+    "absolute_url": "https://boards.greenhouse.io/reddit/jobs/2036084"
+}, {
+    "company": "reddit",
+    "size": "med",
+    "industry": "tech",
+    "title": "Agency Development Lead",
+    "location": "New York",
+    "absolute_url": "https://boards.greenhouse.io/reddit/jobs/1629470"
+}, {
+    "company": "reddit",
+    "size": "med",
+    "industry": "tech",
+    "title": "Agency Development Partnerships Manager",
+    "location": "New York",
+    "absolute_url": "https://boards.greenhouse.io/reddit/jobs/2008204"
+}, {
+    "company": "birchbox",
+    "size": "med",
+    "industry": "beauty",
+    "title": "Data Analyst",
+    "location": "New York, NY",
+    "absolute_url": "http://www.birchbox.com/about/openings?gh_jid=2033786"
+}, {
+    "company": "birchbox",
+    "size": "med",
+    "industry": "beauty",
+    "title": "Operations Intern - Birchbox UK",
+    "location": "London",
+    "absolute_url": "http://www.birchbox.com/about/openings?gh_jid=1968614"
+}, {
+    "company": "birchbox",
+    "size": "med",
+    "industry": "beauty",
+    "title": "Part-Time Discovery Specialist (Temp)",
+    "location": "New York",
+    "absolute_url": "http://www.birchbox.com/about/openings?gh_jid=2049219"
+}, {
+    "company": "birchbox",
+    "size": "med",
+    "industry": "beauty",
+    "title": "Senior Software Engineer (Frontend) ",
+    "location": "New York",
+    "absolute_url": "http://www.birchbox.com/about/openings?gh_jid=2027284"
+}, {
+    "company": "gitlab",
+    "size": "lg",
+    "industry": "tech",
+    "title": "Area Sales Manager (Enterprise), Chicago, Houston, or Dallas",
+    "location": "Remote - Chicago, Houston, or Dallas",
+    "absolute_url": "https://boards.greenhouse.io/gitlab/jobs/4510056002"
+}, {
+    "company": "gitlab",
+    "size": "lg",
+    "industry": "tech",
+    "title": "Area Sales Manager, Enterprise (Northern Europe)",
+    "location": "Remote",
+    "absolute_url": "https://boards.greenhouse.io/gitlab/jobs/4456264002"
+}, {
+    "company": "gitlab",
+    "size": "lg",
+    "industry": "tech",
+    "title": "Area Sales Manager, Enterprise (Southern Europe)",
+    "location": "Remote",
+    "absolute_url": "https://boards.greenhouse.io/gitlab/jobs/4615919002"
+}];
+
 
 window.addEventListener('load', function () {
     new Vue({
         el: '#app',
-        data: () => ({
-          search: null,
-          column: null,
-          items: []
-        }),
-        beforeMount () {
-          this.items = Array.from(Array(20), (x,i) => {
-            return {
-              id: i,
-              name: Math.random().toString(36).substring(7),
-              title: Math.random().toString(36).substring(7),
-              description: Math.random().toString(36).substring(7)
-            }
-          })
+        data: {
+            search: null,
+            search2: null,
+            city: null,
+            cities: ["San Francisco",
+                "New York",
+                "Chicago",
+                "Remote"]
         },
         computed: {
-          cols () {
-            return this.items.length >= 1 ? Object.keys(this.items[0]) : []
-          },
-          rows () {
-            if (!this.items.length) {
-              return []
+            cols() {
+                return JOBS.length >= 1 ? Object.keys(JOBS[0]) : []
+            },
+            colHeaders() {
+                return this.cols.map(c => c.charAt(0).toUpperCase() + c.slice(1))
+            },
+            rows() {
+                if (!JOBS.length) {
+                    return []
+                }
+                if (!this.search && !this.search2) {
+                    return JOBS
+                }
+
+                return JOBS.filter(job => {
+                    var titleContains = !this.search || this.search && job['title'].toLowerCase().includes(this.search.toLowerCase())
+                    var locationContains = !this.search2 || this.search2 && job['location'].toLowerCase().includes(this.search2.toLowerCase())
+                    
+                    return titleContains && locationContains
+                })
             }
-            
-            return this.items.filter(item => {
-              let props = (this.search && this.column) ? [item[this.column]] : Object.values(item)
-              
-              
-              return props.some(prop => !this.search || ((typeof prop === 'string') ? prop.includes(this.search) : prop.toString(10).includes(this.search)))
-            })
-          }
         }
-      })
+    })
 });
 
 // window.addEventListener('load', function () {
